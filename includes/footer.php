46845dc430f6950a5530b56d64460b694b03f692
elbin2899 <elbin2899@gmail.com>
2025-05-04 13:52:58 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -33,7 +33,7 @@
                     <div class="col-lg-3 col-md-6">
                         <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Boletín de
                             Noticias</h4>
-                        <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                        <p>¡Cuéntanos que opinas de nuestro Restaurante!.</p>
                         <div class="position-relative mx-auto" style="max-width: 400px;">
                             <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text"
                                 placeholder="Tu correo electrónico">
