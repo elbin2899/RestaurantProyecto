4ead5fdd3bb86235c242c1d78f0ef1cc69f5bcce
elbin2899 <elbin2899@gmail.com>
2025-05-23 14:37:24 +0200

    Update service_seccion.php
    cambio de la seccion servicio convirtiendolo en botones que llevan a cada seccion

--- seccion/service_seccion.php
+++ seccion/service_seccion.php
@@ -1,50 +1,65 @@
 <!-- Service Start -->
 <section id="service">
-<div class="container-xxl py-5">
-            <div class="container">
-                <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h5 class="section-title ff-secondary text-center text-primary fw-normal">Nuestros Servicios</h5>
-                    <h1 class="mb-5"> Lo que ofrecemos</h1>
-                </div>
-                <div class="row g-4">
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="service-item rounded pt-3">
-                            <div class="p-4">
-                                <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
-                                <h5>Chefs Expertos</h5>
-                        <p>Nuestros chefs combinan experiencia y creatividad para ofrecer platos únicos.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="service-item rounded pt-3">
-                            <div class="p-4">
-                                <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
-                                <h5>Comida de Calidad</h5>
-                        <p>Utilizamos ingredientes frescos y de temporada para garantizar el mejor sabor en cada plato.</p>
-                    </div>
-                </div>
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h5 class="section-title ff-secondary text-center text-primary fw-normal">Nuestros Servicios</h5>
+                <h1 class="mb-5"> Lo que ofrecemos</h1>
             </div>
-            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="service-item rounded pt-3">
-                    <div class="p-4">
-                        <i class="fa fa-3x fa-calendar-check text-primary mb-4"></i>
-                        <h5>Reservas Online</h5>
-                        <p>Realiza tu reserva desde casa de forma rápida, sencilla y segura con nuestra plataforma web.</p>
-                            </div>
+            <div class="row g-4">
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item rounded pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
+                            <a href="index.php#team"
+                                class="nav-item nav-link <?php if($currentPage == 'team.php') echo 'active'; ?>">
+                                <h5>Chefs Expertos</h5>
+                                <p>Nuestros chefs combinan experiencia y creatividad para ofrecer platos únicos.</p>
+                            </a>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                        <div class="service-item rounded pt-3">
-                            <div class="p-4">
-                                <i class="fa fa-3x fa-headset text-primary mb-4"></i>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="service-item rounded pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
+                            <a href="index.php#menu"
+                                class="nav-item nav-link <?php if($currentPage == 'menu.php') echo 'active'; ?>">
+                                <h5>Comida de Calidad</h5>
+                                <p>Utilizamos ingredientes frescos y de temporada para garantizar el mejor sabor en cada
+                                    plato.</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="service-item rounded pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-calendar-check text-primary mb-4"></i>
+                            <a href="index.php#booking"
+                                class="nav-item nav-link <?php if($currentPage == 'booking.php') echo 'active'; ?>">
+                                <h5>Reservas Online</h5>
+                                <p>Realiza tu reserva desde casa de forma rápida, sencilla y segura con nuestra
+                                    plataforma
+                                    web.</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="service-item rounded pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-headset text-primary mb-4"></i>
+                            <a href="contact.php"
+                                class="nav-item nav-link <?php if($currentPage == 'contact.php') echo 'active'; ?>">
                                 <h5>Atención lun-dom</h5>
-                        <p>Estamos disponibles en todo momento para ayudarte con cualquier consulta.</p>
-                            </div>
+                                <p>Estamos disponibles en todo momento para ayudarte con cualquier consulta.</p>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Service End -->
+    </div>
+    <!-- Service End -->
 </section>
