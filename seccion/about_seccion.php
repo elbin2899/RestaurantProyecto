cab2e26d77fa1a2fbd76e6f2399871c099c834de
Gerardo <jafet409@gmail.com>
2025-05-30 19:37:37 +0200

    Quitado botón 'Leer más' de seccion sobre nosotros

--- seccion/about_seccion.php
+++ seccion/about_seccion.php
@@ -44,7 +44,6 @@
                         </div>
                     </div>
                 </div>
-                <a class="btn btn-primary py-3 px-5 mt-2" href="#">Leer Más</a>
             </div>
         </div>
     </div>
